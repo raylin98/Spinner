fd24b25d0889f0bf9074b0e38e86d7d004499bfc
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-13 23:16:50 -0700

    removed extra unnecessary lines from code

--- spinner1.js
+++ spinner1.js
@@ -1,47 +1,47 @@
 
 
 setTimeout(() => {
-  process.stdout.write('\r|   ');
+  process.stdout.write('\r| ');
 }, 100);
 
 setTimeout(() => {
-  process.stdout.write('\r/   ');
+  process.stdout.write('\r/ ');
 }, 300);
 
 setTimeout(() => {
-  process.stdout.write('\r-   ');
+  process.stdout.write('\r- ');
 }, 500);
 
 setTimeout(() => {
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\ '); 
 }, 700);
 
 setTimeout(() => {
-  process.stdout.write('\r|  ');
+  process.stdout.write('\r| ');
 }, 900);
 
 setTimeout(() => {
-  process.stdout.write('\r/  ');
+  process.stdout.write('\r/ ');
 }, 1100);
 
 setTimeout(() => {
-  process.stdout.write('\r-  ');
+  process.stdout.write('\r- ');
 }, 1300);
 
 setTimeout(() => {
-  process.stdout.write('\r\\  ');
+  process.stdout.write('\r\\ ');
 }, 1500);
 
 setTimeout(() => {
-  process.stdout.write('\r|  ');
+  process.stdout.write('\r| ');
 }, 1700);
 
 setTimeout(() => {
-  process.stdout.write('\r/  ');
+  process.stdout.write('\r/ ');
 }, 1900);
 
 setTimeout(() => {
-  process.stdout.write('\r-  ');
+  process.stdout.write('\r- ');
 }, 2100);
 
 setTimeout(() => {
@@ -49,13 +49,13 @@ setTimeout(() => {
 }, 2300);
 
 setTimeout(() => {
-  process.stdout.write('\r|  ');
+  process.stdout.write('\r| ');
 }, 2500);
 
 setTimeout(() => {
-  process.stdout.write('\r/  ');
+  process.stdout.write('\r/ ');
 }, 2700);
 
 setTimeout(() => {
-  process.stdout.write(('\r-  '));
+  process.stdout.write(('\r- '));
 }, 2900);
